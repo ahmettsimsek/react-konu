fcb4cd269b6fe96ebe60f4082cf49620143be47c
ahmettsimsek <ahmtsmsek7@gmail.com>
2026-03-30 00:57:31 +0300

    Gün 2

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import Deneme from "./components/deneme"
 
 function App() {
 
-  const clickFunc = () => {
-      console.log("tıkladınızzzzzzzz")
-  }
+  const arr = [
+    {name: 'array1', id:0},
+    {name: 'array2', id:1},
+    {name: 'array2', id:2},
+  ]
 
   return (
     <div className="App"> 
 
-      <button onClick={clickFunc}>tıkla</button>
+    {
+      arr.map((ar,i) => (
+        <div key={i}>{ar.name}</div> // eğer i kullanmassan map de direk ar.id de olur 
+      ))
+    }
 
     </div>
   );
 }
-// click özelliği için, onclick = {x} ile sağlarız. Yanı tıklanaınca x fonksiyonunu çalıştır bana derim.  
+// Map kavramı, React da bazı şeyleri ekrana map etmek yani yazdırmak için kullanılır. İlk olarak bir array oluştururuz. Ben console log ile yazdırırsam görürüm elaemanları ancak ben ekrana bastırmak istiyorum nasıl yaparım dersem Map ile yaparım.
 
-// js'sel kodlar yani onclick üzerinde tanımlayacağım kodları ben return üzerinde tanımlamam gerek aslında jsx kulanıyorum demek yanlış olmaz. 
+// arrayımız bir değişken ile oluşturulduğu için doğrudan ismi ile çağıramayız unutma, süslü parantez ile  yaparız. süslü parantez bizim yapmak istediğimiz her işlemi div veya span içerisinde yapmamızı sağlar. 
+
+// yazım şekli {değişkenismi.map ((atamaisim,index) => ( <div></div>))}
+
+// Oluşturduğumuz array içerisini tek tek index index gexiyorum, arrayin her bir elemanını alıyorum atamaisme aktarıyorum , aldığım her elemanın isimlerini yazdırmış olurum. 
 export default App;
