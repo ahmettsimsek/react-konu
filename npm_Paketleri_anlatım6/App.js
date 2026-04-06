f0ae702ef84dd4afc8848378984c7fa5964e6011
ahmettsimsek <ahmtsmsek7@gmail.com>
2026-04-06 22:05:57 +0300

    Gün 4

--- npm_Paketleri_anlatım6/App.js
+++ npm_Paketleri_anlatım6/App.js
@@ -16,4 +16,8 @@ export default App;
 
 // Örneğin ikon paketini npm yaparsam, google da react-icons yazarak web siteye gidiyoruz. (npm install react-icons --save) ile paketleri indiriyoruz. ardından ısttediğimiz olan ikonu sayfamızda çagırarak yazıyoruz. 
 
+// Örneğin, goggle giderek react-slick npm edersek (npm install react-slick --save), package.json içerisindeki dependencies kısmı altına react-slick gelmiş olacaktır. Yani bir npm paketini kurmak istediğimizde bu paketi npm install ile kurmak istediğimizde onu  otomatik buraya ekler.
+
+// npm ettikten sonra sayfa bize diyor ki bunun bir de CSS için de bunu kurmalısın diyor. (npm install slick-carousel --save)
+
 // yani kullanmak istediğimiz kütüphaneleri bu şekilde kullanıyoruz. 
